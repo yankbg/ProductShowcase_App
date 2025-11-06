493363891230c1c4dc914b89e6ada6b6a8fe921b
YANNICK kabunga <yankbg2@gmail.com>
2025-11-06 11:38:05 +0300

    implementation of array for different product image

--- app/src/main/java/com/example/task08/MainActivity.java
+++ app/src/main/java/com/example/task08/MainActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final Integer[] images = { R.drawable.straw,
-            R.drawable.banana, R.drawable.orange, R.drawable.mango, R.drawable.apple, R.drawable.watermelon,
-            R.drawable.grape, R.drawable.pineapple};
+    public static final Integer[] images = { R.drawable.sl190,
+            R.drawable.sl280, R.drawable.sl300, R.drawable.sl350, R.drawable.sl500_1998, R.drawable.sl500,
+            R.drawable.slr231, R.drawable.sl63};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
